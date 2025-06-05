ffc23c6f6872b41490e1a4223fc2b5e3808533cb
maaurimiranda <mirandamauri242@gmail.com>
2025-06-05 03:25:43 -0300

    Agregue lógica de los servicios de ABM Rol y DTOs relacionados. Faltan los controladores

--- backend/src/main/java/com/tan/seminario/backend/Repository/RolRepository.java
+++ backend/src/main/java/com/tan/seminario/backend/Repository/RolRepository.java
@@ -4,10 +4,13 @@ import com.tan.seminario.backend.Entity.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 
 public interface RolRepository extends BaseRepository<Rol, Long>{
+    // Verificar si el Rol existe
+    List<Rol> findByNombre(String nombre);
 }
 
