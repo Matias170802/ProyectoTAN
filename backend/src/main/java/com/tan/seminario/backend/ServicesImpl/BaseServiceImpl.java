72cfa20a1b75cd42c78bd71c2fd5fb2384b81d27
maaurimiranda <mirandamauri242@gmail.com>
2025-06-05 03:25:43 -0300

    Agregue lógica de los servicios de ABM Rol y DTOs relacionados. Faltan los controladores

--- backend/src/main/java/com/tan/seminario/backend/ServicesImpl/BaseServiceImpl.java
+++ backend/src/main/java/com/tan/seminario/backend/ServicesImpl/BaseServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> implements BaseService<E, ID> {
+
     protected BaseRepository <E ,ID> baseRepository;
 
     public  BaseServiceImpl(BaseRepository<E,ID> baseRepository) {
@@ -23,9 +24,11 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
 // indicamos que es una transaccion asique cada vez que se ejecute hace un commit un push un todo y un rollback si ocurre un error
     public List<E> findAll() throws Exception {
         try {
+            // find all entities in the repository
             List<E> entities = baseRepository.findAll();
             return entities;
         }catch (Exception e){
+            // throw an exception if an error occurs
             throw new Exception(e.getMessage());
         }
     }
@@ -34,9 +37,11 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     @Transactional
     public E findById(ID id) throws Exception {
         try {
+            // find an entity by its id
             Optional<E> entityOpcional = baseRepository.findById(id);
             return entityOpcional.get();
         }catch (Exception e){
+            // throw an exception if an error occurs
             throw new Exception(e.getMessage());
         }
     }
@@ -45,9 +50,11 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     @Transactional
     public E save(E entity) throws Exception {
         try {
+            // save an entity to the repository
             entity = baseRepository.save(entity);
             return entity;
         }catch (Exception e){
+            // throw an exception if an error occurs
             throw new Exception(e.getMessage());
         }
     }
@@ -56,11 +63,14 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     @Transactional
     public E update(ID id, E entity) throws Exception {
         try {
+            // find an entity by its id
             Optional<E> entityOpcional = baseRepository.findById(id);
+            // update the entity
             E entityUdtate = entityOpcional.get();
             entityUdtate = baseRepository.save(entity);
             return entityUdtate;
         }catch (Exception e){
+            // throw an exception if an error occurs
             throw new Exception(e.getMessage());
         }
     }
@@ -69,13 +79,17 @@ public abstract class BaseServiceImpl<E extends Base, ID extends Serializable> i
     @Transactional
     public boolean delete(ID id) throws Exception {
         try {
+            // check if the entity exists
             if (baseRepository.existsById(id)){
+                // delete the entity
                 baseRepository.deleteById(id);
                 return true;
             }else {
+                // throw an exception if the entity does not exist
                 throw new Exception();
             }
         }catch (Exception e){
+            // throw an exception if an error occurs
             throw new Exception(e.getMessage());
         }
     }
--- backend/src/main/java/com/tan/seminario/backend/ServicesImpl/RolServiceImpl.java
+++ backend/src/main/java/com/tan/seminario/backend/ServicesImpl/RolServiceImpl.java
@@ -1,4 +1,98 @@
 package com.tan.seminario.backend.ServicesImpl;
 
-public class RolServiceImpl {
+import com.tan.seminario.backend.CasosDeUsos.Seguridad.ABMRol.DTOs.CrearRolRequest;
+import com.tan.seminario.backend.CasosDeUsos.Seguridad.ABMRol.DTOs.ModificarRolRequest;
+import com.tan.seminario.backend.Entity.Rol;
+import com.tan.seminario.backend.Repository.RolRepository;
+import com.tan.seminario.backend.Services.RolService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public class RolServiceImpl implements RolService {
+
+    //Repositorio del Rol
+    private final RolRepository rolRepository;
+
+    //Constructor
+    public RolServiceImpl (RolRepository rolRepository) {
+        this.rolRepository = rolRepository;
+    }
+
+    //ALTA
+    @Transactional
+    public void crearRol (CrearRolRequest request){
+        List<Rol> roles = rolRepository.findByNombre(request.getNombreRol());
+        // Verificar que no exista un Rol igual
+        for (Rol rol : roles) {
+            // Si alguno NO esta dado de baja, el rol ya existe
+            if (rol.getFechaHoraBajaRol() == null) {
+                throw new IllegalArgumentException("El rol ya existe y está activo.");
+            }
+        }
+        // Crear nuevo rol
+        Rol nuevoRol = new Rol();
+        nuevoRol.setNombreRol(request.getNombreRol());
+        nuevoRol.setFechaHoraAltaRol(LocalDateTime.now());
+        nuevoRol.setFechaHoraBajaRol(null); // por claridad, aunque suele ser null por defecto
+
+        // Metodo que guarda el Rol en BD
+        rolRepository.save(nuevoRol);
+    }
+
+    //BAJA
+    @Transactional
+    public void bajaRol(Long id) {
+        // Verificar que el Rol exista
+        Optional<Rol> rolFetched = rolRepository.findById(id);
+        if (rolFetched.isEmpty()) {
+            throw new IllegalArgumentException("El rol no existe");
+        }
+        // Verificar que el Rol no este dado de baja
+        if (rolFetched.get().getFechaHoraBajaRol() != null) {
+            throw new IllegalArgumentException("El rol ya está dado de baja");
+        }
+        // Dar de Baja el Rol
+        Rol rol = rolFetched.get();
+
+        //Seteo la hora de baja y guarda el cambio
+        rol.setFechaHoraBajaRol(LocalDateTime.now());
+        rolRepository.save(rol);
+    }
+
+    //MODIFICACION
+    @Transactional
+    public void modificarRol(ModificarRolRequest request){
+        //Solo puedo modificar el nombre del Rol
+        // Verificar que no exista un Rol con el mismo nombre
+        List<Rol> rolsFetched = rolRepository.findByNombre(request.getNombreRol());
+        if (!rolsFetched. isEmpty()) {
+            for (Rol rol : rolsFetched) {
+                // Si alguno NO esta dado de baja, el rol ya existe
+                if (rol.getFechaHoraBajaRol() == null) {
+                    throw new IllegalArgumentException("El rol ya existe y está activo.");
+                }
+            }
+        }
+        //Setear el nombre y guardar cambios
+        // Traerme el Rol Existente y modificarlo
+        Optional<Rol> rolToModify = rolRepository.findById(request.getIdRol());
+        if (rolToModify.isEmpty()) {
+            throw new IllegalArgumentException("El rol no existe");
+        }
+        if (rolToModify.get().getFechaHoraBajaRol() != null) {
+            throw new IllegalArgumentException("El rol está dado de baja");
+        }
+        Rol rol = rolToModify.get();
+        rol.setNombreRol(request.getNombreRol());
+        rolRepository.save(rol);
+    }
+
+    // LISTAR TODOS LOS ROLES (Para el CU: Asignar Roles)
+    @Transactional ()
+    public List<Rol> listarRoles(){
+        return rolRepository.findAll();
+    }
 }
