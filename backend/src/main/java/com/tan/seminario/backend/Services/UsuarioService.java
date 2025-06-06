6b357fb82730ecc58a6cb898303b0fbe92797c88
maaurimiranda <mirandamauri242@gmail.com>
2025-06-06 00:09:59 -0300

    Primera instancia de Correccion CU ABM Rol

--- backend/src/main/java/com/tan/seminario/backend/Services/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.tan.seminario.backend.Services;
-
-import com.tan.seminario.backend.Entity.Usuario;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UsuarioService extends BaseService<Usuario,Long> {
-
-}
