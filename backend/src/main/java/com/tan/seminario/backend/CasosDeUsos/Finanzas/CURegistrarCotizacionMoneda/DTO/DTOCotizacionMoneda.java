34a21f4ad9c01d2876786a865a0ce8f7f3c5f2c8
claramazuran <mazeliguetama@gmail.com>
2025-09-11 00:33:52 -0300

    Se comienza con el back

---
+++ backend/src/main/java/com/tan/seminario/backend/CasosDeUsos/Finanzas/CURegistrarCotizacionMoneda/DTO/DTOCotizacionMoneda.java
@@ -0,0 +1,7 @@
+package com.tan.seminario.backend.CasosDeUsos.Finanzas.CURegistrarCotizacionMoneda.DTO;
+
+public class DTOCotizacionMoneda {
+    public String nombreMoneda;
+    public Long montoCompra;
+    public Long montoVenta;
+}
---
+++ backend/src/main/java/com/tan/seminario/backend/Repository/CotizacionMonedaHoyRepository.java
@@ -0,0 +1,7 @@
+package com.tan.seminario.backend.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tan.seminario.backend.Entity.CotizacionMonedaHoy;
+
+public interface CotizacionMonedaHoyRepository extends JpaRepository<CotizacionMonedaHoy, Long>{
+}
