b99dfa57d798fcdcdb6046d6a54615ef39bd6aa0
claramazuran <mazeliguetama@gmail.com>
2025-11-20 23:33:35 -0300

    Se agregan estilos y elementos de la pantalla de inicio

--- frontend/tan/src/Inicio.tsx
+++ frontend/tan/src/Inicio.tsx
@@ -10,6 +10,15 @@ import { useState } from 'react'
 function AppContent() {
   //TODO: traer las tareas con el useStateInicio o al AppContext
   const [] = useState();
+  const tareas = [
+    {
+      nombreTarea: "Check-in: Apartamento Centro",
+      fechaTarea: "15/04/25",
+      horaTarea: "14:00", 
+      ubicacionTarea: "Apartamento Centro, Calle Principal 123",
+      tipoTarea: "Check-in"
+    }
+  ]
 
   return (
     <main className="min-h-screen bg-gray-100" id='mainPageInicio'>
@@ -18,10 +27,22 @@ function AppContent() {
 
       <section id='contenedorPrincipalTareasAsignadas'>
 
-        <p>Tareas Asignadas</p>
+        <p id='tituloTareasAsignadas'>Tareas Asignadas</p>
 
-        <div id='contenedorSecundarioTareasAsignadas'>
-          //TODO: logica de que por cada tarea crear un nuevo item en la lista de tareas
+        <div id='contenedorItemsTareasAsignadas'>
+          {//TODO: logica de que por cada tarea crear un nuevo item en la lista de tareas     
+          }
+
+          {tareas && tareas.length >0 && (
+            tareas.map((tarea,index) => (
+              <div key={index} className='itemTareaAsignada'>
+                <p className='nombreTareaAsignada'>{tarea.nombreTarea}</p>
+                <p className='detalleTareaAsignada'>Fecha: {tarea.fechaTarea} - Hora: {tarea.horaTarea}</p>
+                <p className='detalleTareaAsignada'>Ubicación: {tarea.ubicacionTarea}</p>
+                <p className='detalleTareaAsignada'>Tipo de Tarea: {tarea.tipoTarea}</p>
+              </div>
+            ))
+          )}
         </div>
       </section>
       
