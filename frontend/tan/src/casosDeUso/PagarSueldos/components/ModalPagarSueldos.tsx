e73a7c8ac5ed5b59feef42c79c0f2ab446e55d33
claramazuran <mazeliguetama@gmail.com>
2025-12-08 13:36:24 -0300

    Se comienza con el front de pagar sueldos

--- frontend/tan/src/casosDeUso/PagarSueldos/components/ModalPagarSueldos.tsx
+++ frontend/tan/src/casosDeUso/PagarSueldos/components/ModalPagarSueldos.tsx
@@ -1,8 +1,13 @@
 import './ModalPagarSueldos.css';
 import {type PropsModalPagarSueldos} from './ModalPagarSueldosTypes'
 import { Modal } from '../../../generalComponents/index';
+import { usePagarSueldos } from '../hooks/usePagarSueldos';
+
 
 export const ModalPagarSueldos: React.FC<PropsModalPagarSueldos> = ({cajaMadre, onClose, isOpen, showCloseButton, children}) => {
+
+    const {empleados, loadingEmpleados, refetchEmpleados, errorEmpleados} = usePagarSueldos();
+
 
     return (
         <Modal
@@ -13,8 +18,26 @@ export const ModalPagarSueldos: React.FC<PropsModalPagarSueldos> = ({cajaMadre, 
         description='Seleccione a un empleado al cual desea pagarle el sueldo'
         >
 
-        <section>
+        <section id='contenedorContenidoPagarSueldos'>
+            <div id='selectEmpleado'>
+                <label>Empleado</label>
 
+                <select>
+                    <option value={"SeleccioneUnEmpelado"}>Seleccione un empleado</option>
+                    {!loadingEmpleados && !errorEmpleados && empleados && empleados.map((empleado) => (
+                        <option key={empleado.dniEmpleado} value={empleado.dniEmpleado}>
+                            {empleado.nombreEmpleado}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div id='contenedorSueldoEmpleado'>
+                <p>Sueldo a pagar: </p>
+                <span>
+                    {//*hacer un watch del select para poder leer el sueldo del empleado seleccionado}
+                </span>
+            </div>
         </section>
 
         </Modal>
