8f30d7bd12916e48db2fa760343385463c078ad9
claramazuran <mazeliguetama@gmail.com>
2025-12-08 13:36:24 -0300

    Se comienza con el front de pagar sueldos

--- frontend/tan/src/casosDeUso/PagarSueldos/hooks/usePagarSueldos.ts
+++ frontend/tan/src/casosDeUso/PagarSueldos/hooks/usePagarSueldos.ts
@@ -1,3 +1,22 @@
-const usePagarSueldos = () => {
-    
+import { useFetch } from "@/generalHooks/useFetch";
+
+interface Empleado {
+
+    dniEmpleado: string;
+    nombreEmpleado: string;
+    sueldoEmpleado: number
+}
+
+
+export const usePagarSueldos = () => {
+
+    const {data: empleados, loading: loadingEmpleados, refetch: refetchEmpleados, error: errorEmpleados} = useFetch<Empleado[]>('/api/finanzas/pagarSueldos');
+
+
+    return {
+        empleados,
+        loadingEmpleados,
+        refetchEmpleados,
+        errorEmpleados
+    }
 }
