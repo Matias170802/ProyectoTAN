99a523c9e531199b9d493138a3301759bf50b93b
maaurimiranda <mirandamauri242@gmail.com>
2025-12-23 22:05:39 -0300

    Resolver conflicto en Inmueble Repository

--- backend/src/main/java/com/tan/seminario/backend/Repository/InmuebleRepository.java
+++ backend/src/main/java/com/tan/seminario/backend/Repository/InmuebleRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 
 public interface InmuebleRepository extends JpaRepository<Inmueble, Long> {
     Inmueble findByCodInmueble(String codInmueble);
+    List<Inmueble> findByFechaHoraBajaInmuebleIsNull();
+    Optional<Inmueble> findByIdAndFechaHoraBajaInmuebleIsNull(Long id);
 }
